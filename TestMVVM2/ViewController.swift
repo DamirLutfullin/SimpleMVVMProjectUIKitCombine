221d836bce0d58dfc9e935bd5157564ab567f26c
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-11-13 07:17:38 +0500

    simple MVC project

--- TestMVVM2/ViewController.swift
+++ TestMVVM2/ViewController.swift
@@ -7,11 +7,36 @@
 
 import UIKit
 
+struct Profile {
+    let name: String
+    let lastname: String
+    let age: Int
+}
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var lastLabel: UILabel!
+    @IBOutlet var ageLabel: UILabel!
+    
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            self.nameLabel.text = profile.name
+            self.lastLabel.text = profile.lastname
+            self.ageLabel.text = profile.age.description
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profile = Profile(name: "IIIIIgor'", lastname: "Da mama", age: 33)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            self.profile = Profile(name: "AAA", lastname: "Foo", age: 42)
+        }
     }
 
 
